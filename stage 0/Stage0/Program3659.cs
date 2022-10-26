0ca537b1c7cef63f60a5423f57ef9ce4dda67a21
saraleShasha <saraleSha@outlook.co.il>
2022-10-26 17:02:12 +0300

    my new function

---
+++ stage 0/Stage0/Program3659.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil0
+{
+    partial class Program
+    {
+      
+        static partial void Welcome3659()
+        {
+            Console.WriteLine("I am also here");
+        }
+    }
+}
