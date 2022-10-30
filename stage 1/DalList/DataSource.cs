0ec1007405e50570dd3e5b84477bb6d48b5403a8
YehuditSarale <projectys@outlook.com>
2022-10-30 20:09:57 +0200

    finalllll

--- stage 1/DalList/DataSource.cs
+++ stage 1/DalList/DataSource.cs
@@ -14,8 +14,7 @@ internal static class DataSource
     static string[] customersNames = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
     static string[] customersEmails = { "A@a", "B@b", "C@c", "D@d", "E@e", "F@f", "G@g", "H@h", "I@i", "J@j", "K@k", "L@l", "M@m", "N@n", "O@o", "P@p", "Q@q", "R@r", "S@s", "T@t", "U@u", "V@v", "W@w", "X@x", "Y@y", "Z@z" };
     static string[] customersAdresses = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
-    static string[] ProductName = {"dresses", "shirts", "pants", "shoes",
-        "skirts", "socks"}
+    static string[] ProductName = { "dresses", "shirts", "pants", "shoes", "skirts", "socks", "sweaters", "tights", "vests", "coats" }
 
     static DataSource() { s_Initialize(); }
     static private void s_Initialize()
@@ -31,29 +30,10 @@ internal static class DataSource
             Product newProduct = new Product();
             newProduct.ID = config.ProductId;
             newProduct.Name = ProductName[i % 10];
-            newProduct.Price = (int)rand NextInt64(50, 450)///(int)
+            newProduct.Price = (int)rand NextInt64(50, 450)
             newProduct.Category = (Category)(i % 5);
             newProduct.InStock = (int)rand NextInt64(1, 50)
             ProductsList[config.indexProduct++] = newProduct;
-
-    static string[] ProductName = {"dresses", "shirts", "pants", "shoes", 
-        "skirts", "socks", "sweaters", "tights", "vests", "coats"}
-
-
-    public void CreateOrdersList
-    { 
-        TimeSpan shipDate = TimeSpan.FromDays(6);
-        TimeSpan deliveryDate = TimeSpan.FromDays(10);
-        for (int i = 0; i < 20; i++){
-            Order newOrder = new Order();
-            newOrder.ID = config.orderId;
-            newOrder.CustomerName = customersNames[i % 26];
-            newOrder.CustomerEmail = customersEmails[i % 26];
-            newOrder.CustomerAdress = customersAdresses[i % 26];
-            newOrder.OrderDate = DateTime.MinValue;
-            newOrder.ShipDate = newOrder.OrderDate + shipDate;
-            newOrder.DeliveryDate = newOrder.ShipDate + deliveryDate;
-            Orders[config.indexOrder++] = newOrder;
         }
     }
 
@@ -101,14 +81,14 @@ internal static class config()
 internal static int indexOrder = 0;
 internal static int indexOrderItem = 0;
 
-    private static int productId = 0;
-    public static int ProductId { get { return productId++} }
+private static int productId = 0;
+public static int ProductId { get { return productId++} }
 
-    private static int orderId = 100;
-    public static int OrderId { get { return orderId++} }
+private static int orderId = 100;
+public static int OrderId { get { return orderId++} }
 
-    private static int orderItem = 1000;
-    public static int OrderItem { get { return orderItem++} }
+private static int orderItem = 1000;
+public static int OrderItem { get { return orderItem++} }
 
     }
 }
