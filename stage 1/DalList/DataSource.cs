d43c9dcb27e24d87c1d6b69280f01097217ec673
YehuditSarale <projectys@outlook.com>
2022-10-30 19:59:32 +0200

    final changes

--- stage 1/DalList/DataSource.cs
+++ stage 1/DalList/DataSource.cs
@@ -17,7 +17,7 @@ internal static class DataSource
         "skirts", "socks"}
 
     static DataSource() { s_Initialize(); }
-    static void s_Initialize()
+    static private void s_Initialize()
     {
         CreateProductsList();
         CreateOrdersList();
@@ -36,42 +36,59 @@ internal static class DataSource
             ProductsList[config.indexProduct++] = newProduct;
         }
     }
-    static public void CreateOrderItemList()
+
+    public void CreateOrdersList
     {
-        for (int i = 0; i < 40; i++)
-        {
-            int num = (int)rand NextInt64(1,4);
-            int OrderId=(int)rand NextInt64(config.indexOrderItem)
-            for (int j = 0; j < num; j++)
-            {
-                int indexProduct = (int)rand.NextInt64(ProductsList.Length);
-                OrderItem newOrderItems = new OrderItem();
-                newOrderItems.ID = Config.OrderItemID;
-                newOrderItems.ProductID = ProductList[indexProduct].ID;
-                newOrderItems.OrderID = Orders[indexOrder].ID;
-                newOrderItems.Amount = (int)rand.NextInt64(1, ProductList[indexProduct].InStock);
-                newOrderItems.Price = (ProductList[indexProduct].Price) * newOrderItems.Amount;
-                OrderItems[config.indexOrderItem++] = newOrderItems;
-
-
-            }
+        TimeSpan shipDate = TimeSpan.FromDays(6);
+        TimeSpan deliveryDate = TimeSpan.FromDays(10);
+        for (int i = 0; i< 20; i++){
+            Order newOrder = new Order();
+            newOrder.ID = config.orderId;
+            newOrder.CustomerName = customersNames[i % 26];
+            newOrder.CustomerEmail = customersEmails[i % 26];
+            newOrder.CustomerAdress = customersAdresses[i % 26];
+            newOrder.OrderDate = DateTime.MinValue;
+            newOrder.ShipDate = newOrder.OrderDate + shipDate;
+            newOrder.DeliveryDate = newOrder.ShipDate + deliveryDate;
+            Orders[config.indexOrder++] = newOrder;
         }
     }
+    static public void CreateOrderItemList()
+{
+    for (int i = 0; i < 40; i++)
+    {
+        int num = (int)rand NextInt64(1, 4);
+        int OrderId = (int)rand NextInt64(config.indexOrderItem)
+            for (int j = 0; j < num; j++)
+        {
+            int indexProduct = (int)rand.NextInt64(ProductsList.Length);
+            OrderItem newOrderItems = new OrderItem();
+            newOrderItems.ID = Config.OrderItemID;
+            newOrderItems.ProductID = ProductList[indexProduct].ID;
+            newOrderItems.OrderID = Orders[indexOrder].ID;
+            newOrderItems.Amount = (int)rand.NextInt64(1, ProductList[indexProduct].InStock);
+            newOrderItems.Price = (ProductList[indexProduct].Price) * newOrderItems.Amount;
+            OrderItems[config.indexOrderItem++] = newOrderItems;
 
-    internal static class config()
+        }
+    }
+}
+
+
+internal static class config()
     {
     internal static int indexProduct = 0;
-    internal static int indexOrder = 0;
-    internal static int indexOrderItem = 0;
+internal static int indexOrder = 0;
+internal static int indexOrderItem = 0;
 
-    private static int productId = 0;
-    public static int ProductId { get { return productId++} }
+private static int productId = 0;
+public static int ProductId { get { return productId++} }
 
-    private static int orderId = 100;
-    public static int OrderId { get { return orderId++} }
+private static int orderId = 100;
+public static int OrderId { get { return orderId++} }
 
-    private static int orderItem = 1000;
-    public static int OrderItem { get { return orderItem++} }
+private static int orderItem = 1000;
+public static int OrderItem { get { return orderItem++} }
 
 }
 }
