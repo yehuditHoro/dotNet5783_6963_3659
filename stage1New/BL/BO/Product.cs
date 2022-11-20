b1fa59110f06fa13b519c1abf5722378dd24b913
YehuditSarale <projectys@outlook.com>
2022-11-20 15:45:30 +0200

    my changes1

---
+++ stage1New/BL/BO/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    internal class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public BO.Enums.eCategory Category { get; set; }
+        public int InStock { get; set; }
+
+
+       public override string ToString() => $@"
+       Product ID: {ID}, 
+       Name: {Name},
+       Price: {Price},
+       Category: {Category},
+       Amount in stock: {InStock}";
+    }
+}
