0815579710912de484e178da4527f0664899b289
saraleShasha <saraleSha@outlook.co.il>
2022-11-20 15:48:51 +0200

    stage 2

--- stage1New/BL/BO/OrderForList.cs
+++ stage1New/BL/BO/OrderForList.cs
@@ -1,25 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+namespace BO;
 
-namespace BO
+internal class OrderForList
 {
-    internal class OrderForList
-    {
-        public int ID { get; set; }
-        public string CustomerName { get; set; }
-        public BO.Enums.OrderStatus Status { get; set; }   
-        public int AmountOfItems { get; set; }
-        public double TotalPrice { get; set; }
+    public int ID { get; set; }
+    public string CustomerName { get; set; }
+    public BO.Enums.OrderStatus Status { get; set; }
+    public int AmountOfItems { get; set; }
+    public double TotalPrice { get; set; }
 
-        public override string ToString() => $@"
+    public override string ToString() => $@"
        ID: {ID}, 
        Customer name: {CustomerName},
        Status: {Status},
        Amount Of Items {AmountOfItems},
        Total price: {TotalPrice}";
 
-    }
 }
+
+
