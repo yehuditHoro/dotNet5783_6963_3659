a019d208ba5886e8b7cb094a96c712fc4593354b
visual studio <vs@mbjcomp.org.il>
2022-11-23 12:39:30 +0200

    vvvvv

--- stage1New/BL/BlImplementation/BlCart.cs
+++ stage1New/BL/BlImplementation/BlCart.cs
@@ -1,9 +1,25 @@
-﻿
+﻿using DalApi;
+using dalList;
+
 namespace BlImplementation;
 
-internal class BlCart
+internal class BlCart : BlApi.Icart
 {
-    Console.WriteLine(//);
+    IDal Dal = new DalList();
+    public BO.Cart addToCart(BO.Cart c, int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void MakeAnOrder(BO.Cart c, string name, string email, string address)
+    {
+        throw new NotImplementedException();
+    }
+
+    public BO.Cart UpdateQuantity(BO.Cart c, int id, int quantity)
+    {
+        throw new NotImplementedException();
+    }
 }
 
 
--- stage1New/BL/BlImplementation/BlProduct.cs
+++ stage1New/BL/BlImplementation/BlProduct.cs
@@ -1,8 +1,45 @@
-﻿
+﻿using DalApi;
+using dalList;
+
 namespace BlImplementation;
 
-internal class BlProduct
+internal class BlProduct : BlApi.Iproduct
 {
+    IDal Dal = new DalList();
 
+    public void AddProduct(BO.Product p)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<BO.ProductItem> GetCatalog(BO.ProductItem catalog)
+    {
+        throw new NotImplementedException();
+    }
+
+    public BO.Product GetProductItemsForCustomer(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public BO.Product GetProductItemsForManager(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<BO.ProductForList> GetProducts(BO.ProductForList productList)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveProduct(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateProduct(BO.Product p)
+    {
+        throw new NotImplementedException();
+    }
 }
 
