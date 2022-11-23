64e4280d6e3d0fcbc14a1b22ef6a6a1308737dbf
saraleShasha <saraleSha@outlook.co.il>
2022-11-23 18:14:08 +0200

    all changes

--- stage1New/BL/BlImplementation/BlCart.cs
+++ stage1New/BL/BlImplementation/BlCart.cs
@@ -8,13 +8,32 @@ internal class BlCart : BlApi.Icart
     IDal Dal = new DalList();
     public BO.Cart addToCart(BO.Cart c, int pId)
     {
-       foreach (BO.OrderItem oi in c.Items)
+        foreach (BO.OrderItem oi in c.Items)
         {
-            if (oi.ID== pId)
+            if (oi.ID == pId)
             {
-                if(Dal.)
+                if (Dal.product.Read(pId).InStock > 0)
+                {
+                    oi.Amount++;
+                    oi.TotalPrice += oi.Price;
+                    return c;
+                }
             }
-
+        }
+        Dal.DO.Product prod = Dal.product.Read(pId);
+        if (prod.InStock > 0)
+        {
+            BO.OrderItem newP = new BO.OrderItem();
+           
+            newP.ID =(Dal.DataSource.config.OrderItemId);
+            newP.Name = prod.Name;  
+            newP.Price = prod.Price;
+            newP.ProductID = pId;
+            newP.Amount =1;
+            newP.TotalPrice = prod.Price;
+            c.Items.Add(newP);
+            c.TotalPrice +=newP.Price;
+            return c;
         }
         throw new NotImplementedException();
     }
--- stage1New/BL/BlImplementation/BlProduct.cs
+++ stage1New/BL/BlImplementation/BlProduct.cs
@@ -6,10 +6,13 @@ namespace BlImplementation;
 internal class BlProduct : BlApi.Iproduct
 {
     IDal Dal = new DalList();
-
     public IEnumerable<BO.ProductForList> GetProducts()
     {
         IEnumerable<Dal.DO.Product> getProducts = Dal.product.ReadAll();
+        if (getProducts.Count() <= 0)
+        {
+            throw new Exception();
+        }
         List<BO.ProductForList> boProducts = new List<BO.ProductForList>();
         foreach (Dal.DO.Product p in getProducts)
         {
@@ -21,39 +24,113 @@ internal class BlProduct : BlApi.Iproduct
             //bp.InStock = p.InStock;
             boProducts.Add(bp);
         }
-
-        throw new NotImplementedException();
+        return boProducts;
     }
 
     public IEnumerable<BO.ProductItem> GetCatalog()
     {
-        throw new NotImplementedException();
+        IEnumerable<Dal.DO.Product> getCatalog = Dal.product.ReadAll();
+        if (getCatalog.Count() <= 0)
+        {
+            throw new Exception();
+        }
+        List<BO.ProductItem> Catalog = new List<BO.ProductItem>();
+        foreach (Dal.DO.Product p in getCatalog)
+        {
+            BO.ProductItem productItem = new BO.ProductItem();
+            productItem.ID = p.ID;
+            productItem.Name = p.Name;
+            productItem.Price = p.Price;
+            productItem.Category = (BO.Enums.eCategory)p.Category;
+            productItem.InStock = (p.InStock != 0 ? true : false);
+            //productItem.Amount =
+            Catalog.Add(productItem);
+        }
+        return Catalog;
     }
 
     public BO.Product GetProductItemsForManager(int id)
     {
-        throw new NotImplementedException();
+        if (id > 0)
+        {
+            Dal.DO.Product p = Dal.product.Read(id);
+            BO.Product prod = new BO.Product();
+            prod.ID = p.ID;
+            prod.Name = p.Name;
+            prod.Price = p.Price;
+            prod.Category = (BO.Enums.eCategory)p.Category;
+            prod.InStock = p.InStock;
+            return prod;
+        }
+
+        throw new Exception();
     }
     public BO.Product GetProductItemsForCustomer(int id)
     {
-        throw new NotImplementedException();
+        /// לשאול את המורה למה הכפילות?
+        if (id > 0)
+        {
+            Dal.DO.Product p = Dal.product.Read(id);
+            BO.Product prod = new BO.Product();
+            prod.ID = p.ID;
+            prod.Name = p.Name;
+            prod.Price = p.Price;
+            prod.Category = (BO.Enums.eCategory)p.Category;
+            prod.InStock = p.InStock;
+            return prod;
+        }
+
+        throw new Exception();
     }
 
     public void AddProduct(BO.Product p)
     {
-        throw new NotImplementedException();
+        if (p.ID > 0 && p.Name != null && p.Price > 0 && p.InStock > 0)
+        {
+            Dal.DO.Product prod = new Dal.DO.Product();
+            prod.ID = p.ID;
+            prod.Name = p.Name;
+            prod.Price = p.Price;
+            prod.Category = (Dal.DO.eCategory)p.Category;
+            prod.InStock = p.InStock;
+            Dal.product.Add(prod);
+        }
+        else
+        {
+            throw new Exception();
+        }
     }
 
 
 
     public void RemoveProduct(int id)
     {
-        throw new NotImplementedException();
+        IEnumerable<Dal.DO.OrderItem> AllItems = Dal.orderItem.ReadAll();
+        foreach (Dal.DO.OrderItem item in AllItems)
+        {
+            if (item.ID == id)
+                Dal.orderItem.Delete(item.ID);
+        }
+        throw new Exception();
     }
 
     public void UpdateProduct(BO.Product p)
     {
-        throw new NotImplementedException();
+        if (p.ID > 0 && p.Name != null && p.Price > 0 && p.InStock > 0)
+        {
+            Dal.DO.Product prod = new Dal.DO.Product();
+            prod.ID = p.ID;
+            prod.Name = p.Name;
+            prod.Price = p.Price;
+            prod.Category = (Dal.DO.eCategory)p.Category;
+            prod.InStock = p.InStock;
+            Dal.product.Update(prod);
+        }
+        else
+        {
+            throw new Exception();
+        }
     }
 }
+
 
