9587c6de4d7f2950abe5281290480e6f2b644508
YehuditSarale <projectys@outlook.com>
2023-01-01 16:36:45 +0200

    sarale tamshichi behanaha veani elmad ledinim

--- DalXml/DalProduct.cs
+++ DalXml/DalProduct.cs
@@ -3,12 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using Dal.DO;
 using DalApi;
 
 namespace Dal;
 
-    internal class DalProduct : Iproduct
+internal class DalProduct : Iproduct
+{
+    
+    public int Add(Product item)
     {
-
+        throw new NotImplementedException();
     }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Product Read(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Product> ReadAll(Func<Product, bool>? func = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Product ReadSingle(Func<Product, bool> func)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Product item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateAmount(int id, int amount)
+    {
+        throw new NotImplementedException();
+    }
+
+    
+}
 
--- DalXml/DalXml.cs
+++ DalXml/DalXml.cs
@@ -1,4 +1,6 @@
 ﻿using DalApi;
+using Dal;
+using System.Xml.Linq;
 
 namespace DalXml
 {
@@ -7,6 +9,45 @@ namespace DalXml
         public Iproduct product { get; } = new Dal.DalProduct();
         public Iorder order { get; } = new Dal.DalOrder();
         public IorderItem orderItem { get; } = new Dal.DalOrderItem();
+
+
+        public static List<Dal.DO.Product> ProductList = new();
+
+        static readonly Random rand = new Random();
+
+        public static List<Dal.DO.Product> AddListProducts()
+        {
+            XElement? root = XDocument.Load("..\\..\\Product.xml").Root;
+            string[] productNames = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "g" };
+            for (int i = 0; i < 10; i++)
+            {
+                Dal.DO.Product product = new();
+                product.ID = config.ProductId;
+                product.Name = productNames[i % 10];
+                product.Price = (int)rand.Next(50, 450);
+                product.Category = (Dal.DO.eCategory)(i % Enum.GetValues(typeof(Dal.DO.eCategory)).Length);
+                if (i % 20 == 0)
+                    product.InStock = 0;
+                else
+                    product.InStock = (int)rand.NextInt64(0, 1000);
+
+                ProductsList.Add(product);
+            }
+        }
+
+
+        public static class config
+        {
+            private static int _productId = 0;
+            public static int ProductId { get { return _productId++; } }
+
+            private static int _orderId = 100;
+            public static int OrderId { get { return _orderId++; } }
+
+            private static int _orderItemId = 1000;
+            public static int OrderItemId { get { return _orderItemId++; } }
+
+        }
 
     }
 }
